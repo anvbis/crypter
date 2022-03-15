cc822b826e6544e65ca3708c080f97af85cfb643
anvbis <stephen.t.radley+anvbis@protonmail.com>
2022-03-15 16:12:53 +1100

    removed testing code

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -7,18 +7,5 @@
 
 int main(int argc, char **argv)
 {
-    pe_data_t pe_data;
-    pe_data_read(&pe_data, "data/sample.exe");
-
-    pe_data_encrypt(&pe_data, "yellow submarine");
-    pe_data_write(&pe_data, "data/sample.exe.enc");
-
-    pe_data_free(&pe_data);
-    pe_data_read(&pe_data, "data/sample.enc.exe");
-
-    pe_data_decrypt(&pe_data, "yellow submarine");
-    pe_data_write(&pe_data, "data/sample.dec.exe");
-
-    pe_data_free(&pe_data);
     return 0;
 }
