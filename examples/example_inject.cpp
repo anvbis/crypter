0140e3e0aeaa03f3f03157d6781e72c99bbbc18c
anvbis <stephen.t.radley+anvbis@protonmail.com>
2022-03-17 00:34:52 +1100

    started refactor

--- examples/example_inject.cpp
+++ examples/example_inject.cpp
@@ -7,12 +7,12 @@
 int main(int argc, char **argv)
 {
     /* read pe data into memory */
-    pe_data_t pe_data;
-    pe_data_read(&pe_data, "data/sample.exe");
+    crypter::pe_data_t pe;
+    crypter::init(&pe, "data/sample.exe");
 
     /* inject pe via process hollowing */
-    pe_data_inject(&pe_data, "C:\\Windows\\explorer.exe");
+    crypter::inject(&pe, "C:\\Windows\\explorer.exe");
 
-    pe_data_free(&pe_data);
+    crypter::free(&pe);
     return 0;
 }
--- include/crypter.hpp
+++ include/crypter.hpp
@@ -7,28 +7,34 @@
 
 #include <string>
 
-/* stores portable executable data */
-typedef struct {
-    char *bytes;
-    size_t size;
-} pe_data_t;
+namespace crypter {
+    /* stores portable executable data */
+    typedef struct {
+        char *bytes;
+        size_t size;
+    } pe_data_t;
 
-/* reads pe data into memory */
-int pe_data_read(pe_data_t *pe_data, std::string filename);
+    /* reads pe data into memory */
+    int init(pe_data_t *pe, std::string filename);
 
-/* writes pe data out into file */
-int pe_data_write(pe_data_t *pe_data, std::string filename);
+    /* write pe data out into file */
+    int write(pe_data_t *pe, std::string filename);
 
-/* encrypts pe data using repeating key xor */
-void pe_data_encrypt(pe_data_t *pe_data, std::string key);
+    namespace encrypt {
+        /* encrypts pe data using repeating key xor */
+        void xor(pe_data_t *pe, std::string key);
+    }
 
-/* decrypts pe data using repeating key xor */
-void pe_data_decrypt(pe_data_t *pe_data, std::string key);
+    namespace decrypt {
+        /* decrypts pe data using repeating key xor */
+        void xor(pe_data_t *pe, std::string key);
+    }
 
-/* ... */
-int pe_data_inject(pe_data_t *pe_data, char *target);
+    /* executes pe via process hollowing */
+    int inject(pe_data_t *pe, std::string target);
 
-/* frees memory allocated when reading pe data */
-void pe_data_free(pe_data_t *pe_data);
+    /* frees memory allocated when reading pe data */
+    void free(pe_data_t *pe);
+}
 
 #endif /* CRYPTER_HPP */
